83c393bdda0ed278e6461dd29335545e47caa803
Sada <sadacharank@gmail.com>
2023-06-25 14:50:35 +0530

    congigure backend url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { HelmetProvider } from 'react-helmet-async'
 import Equipments from "./components/equipments/equipments";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
@@ -16,53 +17,56 @@ import AuthContextProvider from "./context/authContextProvider";
 import ProtectedRoute from "./route/protectedRoute";
 
 
+
 function App() {
   return (
     <AuthContextProvider>
       <CartContextProvider>
         <Router>
           <div className="App">
-            <Header />
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/student-login" element={<StudentLogin />} />
-              <Route path="/student-register" element={<StudentRegister />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/reset-password" element={<ResetPassword />} />
+            <HelmetProvider>
+              <Header />
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      <Home />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/student-login" element={<StudentLogin />} />
+                <Route path="/student-register" element={<StudentRegister />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/reset-password" element={<ResetPassword />} />
 
-              <Route
-                path="/laboratory"
-                element={
-                  <ProtectedRoute>
-                    <Laboratory />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/laboratory/:labId"
-                element={
-                  <ProtectedRoute>
-                    <Equipments />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="cart"
-                element={
-                  <ProtectedRoute>
-                    <Cart />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-            <Footer />
+                <Route
+                  path="/laboratory"
+                  element={
+                    <ProtectedRoute>
+                      <Laboratory />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/laboratory/:labId"
+                  element={
+                    <ProtectedRoute>
+                      <Equipments />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="cart"
+                  element={
+                    <ProtectedRoute>
+                      <Cart />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+              <Footer />
+            </HelmetProvider>
           </div>
         </Router>
       </CartContextProvider>
